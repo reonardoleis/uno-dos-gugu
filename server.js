73204c60ce25a47f0c6f60772e85657c620345c8
BuildTools <leonardosilvabf@gmail.com>
2020-06-03 13:28:19 -0300

    v0.1.1

--- server.js
+++ server.js
@@ -2,13 +2,22 @@
 const app       = express();
 const http      = require('http').Server(app);
 const io        = require('socket.io')(http);
+const md5       = require('md5');
 const PORT      = 3000;
 
 //servir arquivos da pasta public
 app.use(express.static('public'));
 
 //parte do socket
-
+io.on('connection', function(client) {
+    console.log('Cliente conectado...');
+    console.log(client.id);
+    client.on('novoUsuario', function(apelido) {
+        let novo_id = md5(apelido);
+        console.log(`Usuário ${apelido} gerado com ID ${novo_id}. Enviando dados ao cliente...`);
+        io.to(client.id).emit('infoUsuario', [novo_id, apelido]);
+    });
+});
 
 
 //iniciar server

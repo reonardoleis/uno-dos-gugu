9444425a4586b2c6ddb81014c92632d21eecd2ac
BuildTools <leonardosilvabf@gmail.com>
2020-06-03 12:55:41 -0300

    v0.1

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿const express   = require('express');
+const app       = express();
+const http      = require('http').Server(app);
+const io        = require('socket.io')(http);
+const PORT      = 3000;
+
+//servir arquivos da pasta public
+app.use(express.static('public'));
+
+//parte do socket
+
+
+
+//iniciar server
+http.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
+})
